b1c02dc045f0bec5e0968fd84273487d21cb2d25
carnivalcruz380 <84043530+carnivalcruz380@users.noreply.github.com>
2021-07-05 22:53:15 -0400

    MainActivity(updated all contents of characterDisplay moved to MainActivity) id: MainActivity.java --Overview-- This activity is intended to let users add/store up to 6(can be more) character profiles. By tapping the imageButtons, the user can add a character to an empty profile slot, update a character currently occupying a slot, or delete one of the profiles. After one of these actions, the TextViews under each imageButton will be updated with the name of that character, or revert back to @string/add_character_label
    --funtions--
    - 6 ImageButtons: will allow you to add a character or edit an existing character if it already exists. If no character has been created,
    an intent will be sent to an addCharacter activity(not yet created). Once the character has been created
    the ImageButtons will be updated with the image the user has chosen for that character/empty avatar if no image was uploaded.
    ids: character1Button, character2Button, character3Button, character4Button, character5Button, character6Button. The ids start from the top row
    from left to right, then the bottom row from left to right
    onClickListener: imageListener
    
    - 6 TextViews: will be updated with the name of the characer after the character has been created
    ids: character1Label, character2Label, character3Label, character4Label, character5Label, character6Label. They follow the same naming convention
    as the ImageButtons
    
    play Button: button will create some kind of popup(maybe a dialoge or listView fragment) that will allow you to select which profile to
    start/continue gameplay with and then send you to the play game activity(not yet created)
    id: playButton
    OnClickListener: playListener
    
    - rules Button: button that will take you to the pdf of the rules page
    id: rulesButton
    OnClickListener: rulesListener
    - profile booleans: these will help us to switch to either the character update activity, or character create activity. If profile(x) is
    false, then there is no character associated with the profile. If profile(x) is true, then the user can update/delete the character
    associated with the profile. X is the imageButton associated to the profile boolean (ie boolean profile1 goes to character1Button ect...)

--- app/src/main/java/com/example/fateassist/MainActivity.java
+++ app/src/main/java/com/example/fateassist/MainActivity.java
@@ -9,17 +9,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // main activity buttons
-    Button playButton;      // allows you to continue gameplay on any current characters
-    Button characterButton; // allows you to modity/add characters to your account
-    Button rulesButton;     // takes you to rules pdf
+    // buttons that you can click on to add a new character or view/change character profiles
+    ImageButton character1Button;
+    ImageButton character2Button;
+    ImageButton character3Button;
+    ImageButton character4Button;
+    ImageButton character5Button;
+    ImageButton character6Button;
+
+    // button to start/continue gameplay on any profile
+    Button playButton;
+    // button for the rules pdf display
+    Button rulesButton;
+
+    // text under buttons that will change to the character's name once a character has been added
+    TextView character1Label;
+    TextView character2Label;
+    TextView character3Label;
+    TextView character4Label;
+    TextView character5Label;
+    TextView character6Label;
 
     //adapters for database
     Cursor playerCursor;
     CursorAdapter playerCursorAdapter;
+
+    // booleans for ImageButtons
+    // if false will take you to create new character activity
+    // if true will take you to update character activity
+    static boolean character1 = false;
+    static boolean character2 = false;
+    static boolean character3 = false;
+    static boolean character4 = false;
+    static boolean character5 = false;
+    static boolean character6 = false;
 
     //This may or may not be useful to you, just know that these should be the options
     //for each skill box
@@ -44,23 +72,64 @@ public class MainActivity extends AppCompatActivity {
         Will,
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        character1Button = (ImageButton) findViewById(R.id.character1Button);
+        character2Button = (ImageButton) findViewById(R.id.character2Button);
+        character3Button = (ImageButton) findViewById(R.id.character3Button);
+        character4Button = (ImageButton) findViewById(R.id.character4Button);
+        character5Button = (ImageButton) findViewById(R.id.character5Button);
+        character6Button = (ImageButton) findViewById(R.id.character6Button);
+
         playButton = (Button) findViewById(R.id.playButton);
-        characterButton = (Button) findViewById(R.id.characterButton);
         rulesButton = (Button) findViewById(R.id.rulesButton);
 
-        characterButton.setOnClickListener(characterListener);
+        character1Label = (TextView) findViewById(R.id.character1Label);
+        character2Label = (TextView) findViewById(R.id.character2Label);
+        character3Label = (TextView) findViewById(R.id.character3Label);
+        character4Label = (TextView) findViewById(R.id.character4Label);
+        character5Label = (TextView) findViewById(R.id.character5Label);
+        character6Label = (TextView) findViewById(R.id.character6Label);
+
+        // NOT YET IMPLEMENTED
+        character1Button.setOnClickListener(imageListener);
+        character2Button.setOnClickListener(imageListener);
+        character3Button.setOnClickListener(imageListener);
+        character4Button.setOnClickListener(imageListener);
+        character5Button.setOnClickListener(imageListener);
+        character6Button.setOnClickListener(imageListener);
+
+        // NOT YET IMPLEMENTED
+        playButton.setOnClickListener(playListener);
+
+        // NOT YET IMPLEMENTED
+        rulesButton.setOnClickListener(rulesListener);
     }
 
-    // onclick listener that takes you to character activity
-    View.OnClickListener characterListener = new View.OnClickListener() {
+    // NOT YET IMPLEMENTED
+    View.OnClickListener imageListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, charactersDisplay.class);
-            startActivity(intent);
+
+        }
+    };
+
+    // NOT YET IMPLEMENTED
+    View.OnClickListener playListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    // NOT YET IMPLEMENTED
+    View.OnClickListener rulesListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
         }
     };
 }
