1768f66f105c7def24ff78b07cb48aa2e36f141c
Robby <rwhitwo@gmail.com>
2021-07-21 20:45:15 -0400

    Set up Framework for Stress tracking on PlayActivity.

--- app/src/main/java/com/example/fateassist/PlayActivity.java
+++ app/src/main/java/com/example/fateassist/PlayActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,10 +19,19 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        Button backButton = findViewById(R.id.backButton);
+        Button backButton = findViewById(R.id.backButton);//might not use since android already has one built in
         Button rollButton = (Button) findViewById(R.id.rollButton);
         Spinner diceSpinner = findViewById(R.id.diceSpinner);
         TextView rolledTextView = findViewById(R.id.rolledTextView);
+        CheckBox psBox1 = findViewById(R.id.physicalStressBox1);
+        CheckBox psBox2 = findViewById(R.id.physicalStressBox2);
+        CheckBox psBox3 = findViewById(R.id.physicalStressBox2);
+        CheckBox psBox4 = findViewById(R.id.physicalStressBox4);
+        CheckBox msBox1 = findViewById(R.id.mentalStressBox1);
+        CheckBox msBox2 = findViewById(R.id.mentalStressBox2);
+        CheckBox msBox3 = findViewById(R.id.mentalStressBox3);
+        CheckBox msBox4 = findViewById(R.id.mentalStressBox4);
+
 
         Integer[] diceArray = {20, 12, 10, 8, 6, 4};
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, diceArray);
